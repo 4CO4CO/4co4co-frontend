ad40eb0049d9eff9edb6de0ae9c529669b5aa46c
imddoy <imddoy@duksung.ac.kr>
2025-05-12 03:06:50 +0900

    chore: 페이지 연결

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import App from './App.tsx';
-import MVP from './pages/handInteraction/MVP.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MVP />,
+    <App />,
   </StrictMode>,
 );
