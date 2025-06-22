21402092840dcdbfad2fdb5ad3fb9fc9f4337b45
devdaradara <devdaradara@gmail.com>
2025-06-22 21:12:00 +0900

    chore: 페이지 및 assets 설정

--- src/routes/lazy.ts
+++ src/routes/lazy.ts
@@ -1,5 +1,6 @@
 import { lazy } from 'react';
 
+export const MainPage = lazy(() => import('@/pages/main/Main'));
 export const FormPage = lazy(() => import('@/pages/form/Form'));
 export const LanternPage = lazy(() => import('@/pages/lantern/Lantern'));
 export const LanternDetailPage = lazy(() => import('@/pages/lantern/LanternDetail'));
