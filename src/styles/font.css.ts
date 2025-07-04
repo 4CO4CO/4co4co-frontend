7501e8bdc782edef0a1166c762367a3de27530b1
imddoy <imddoy@duksung.ac.kr>
2025-07-04 20:02:03 +0900

    chore: 디자인 시스템 정의

---
+++ src/styles/font.css.ts
@@ -0,0 +1,65 @@
+import { createGlobalTheme } from '@vanilla-extract/css';
+
+export const fonts = createGlobalTheme(':root', {
+  font: {
+    title_60_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '6rem',
+      fontWeight: '700',
+      letterSpacing: '0.6rem',
+    },
+    title_40_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '4rem',
+      fontWeight: '700',
+      letterSpacing: '0.4rem',
+    },
+    title_28_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '2.8rem',
+      fontWeight: '700',
+      letterSpacing: '0.28rem',
+    },
+    title_18_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.8rem',
+      fontWeight: '700',
+      letterSpacing: '0.18rem',
+    },
+    body_18_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.8rem',
+      fontWeight: '700',
+    },
+    body_16_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.6rem',
+      fontWeight: '700',
+    },
+    body_16_R: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.6rem',
+      fontWeight: '400',
+    },
+    body_15_M: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.5rem',
+      fontWeight: '500',
+    },
+    body_14_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '1.4rem',
+      fontWeight: '700',
+    },
+    body_10_B: {
+      fontFamily: 'Pretendard',
+      fontSize: '1rem',
+      fontWeight: '700',
+    },
+    body_10_R: {
+      fontFamily: 'Pretendard',
+      fontSize: '1rem',
+      fontWeight: '400',
+    },
+  },
+});
