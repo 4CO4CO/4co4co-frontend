b867cf7f74f202b60e79efe1a040cdca65de9b81
imddoy <imddoy@duksung.ac.kr>
2025-07-13 09:24:01 +0900

    feat: lantern 쿼리키 생성

---
+++ src/queries/queryKey.ts
@@ -0,0 +1,7 @@
+export const lanternKeys = {
+  all: ['lantern'] as const,
+  lists: () => [...lanternKeys.all, 'list'] as const,
+  list: (idx: string) => [...lanternKeys.lists(), idx] as const,
+  details: () => [...lanternKeys.all, 'detail'] as const,
+  detail: (id: number) => [...lanternKeys.details(), id] as const,
+};
