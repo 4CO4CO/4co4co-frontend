487dded48ab0afe8ef2b9c0c7006630ba732332e
imddoy <imddoy@duksung.ac.kr>
2025-07-16 01:01:02 +0900

    fix: 코드리뷰 반영

--- src/queries/queryKey.ts
+++ src/queries/queryKey.ts
@@ -3,5 +3,5 @@ export const lanternKeys = {
   lists: () => [...lanternKeys.all, 'list'] as const,
   list: (idx: string) => [...lanternKeys.lists(), idx] as const,
   details: () => [...lanternKeys.all, 'detail'] as const,
-  detail: (id: number) => [...lanternKeys.details(), id] as const,
+  detail: (id: string) => [...lanternKeys.details(), id] as const,
 };
