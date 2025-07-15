426558b43d868bc63a48cf11fc971ed4d3e76d18
또이 <90364711+imddoy@users.noreply.github.com>
2025-07-16 01:28:12 +0900

    init: 쿼리 초기 세팅 추가 (#25)
    * feat: 쿼리클라이언트 추가
    
    * feat: lantern 쿼리키 생성
    
    * chore: 파일구조 설정
    
    * fix: 코드리뷰 반영
    
    * feat: 생성상태 쿼리키 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { queryClient } from './queries/queryClient.ts';
 import './styles/reset.css.ts';
 import './styles/global.css.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />,
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
