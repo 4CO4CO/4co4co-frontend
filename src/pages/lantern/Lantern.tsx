b0a407f4f687ff5fcb7613dd7f9fdf7e6591e7d3
imddoy <imddoy@duksung.ac.kr>
2025-07-25 00:13:06 +0900

    feat: 풍등 조회 쿼리 구현

--- src/pages/lantern/Lantern.tsx
+++ src/pages/lantern/Lantern.tsx
@@ -1,57 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import * as styles from './Lantern.css';
 import { useHandMark } from '../../components/common/lantern/hooks/useHandMark';
 import { VideoFeed } from '../../components/common/lantern/VideoFeed';
-import { get } from '@/apis';
 import { LanternWithRect } from '@/components/common/lantern/constants';
 import { useLanternHit } from '@/components/common/lantern/hooks/useLanternHit';
+import { useLanternList } from '@/queries/lantern/getLanternList';
 
 const Lantern = () => {
   const { handCenter } = useHandMark();
   const [searchParams] = useSearchParams();
   const currentLanternId = searchParams.get('currentLanternId');
-  const [lanterns, setLanterns] = useState<LanternWithRect[]>([]);
-  const { hitLanternId } = useLanternHit(lanterns);
+  const { data } = useLanternList(currentLanternId);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!currentLanternId) {
-      navigate('/');
-      return;
-    }
+  const lanterns = useMemo(() => {
+    return (data?.data ?? []).map((lantern) => ({
+      ...lantern,
+      rect: {
+        x: Math.random() * 600,
+        y: Math.random() * 400,
+        width: 100,
+        height: 100,
+      },
+    })) as LanternWithRect[];
+  }, [data]);
 
-    const fetchLanterns = async () => {
-      try {
-        const response = await get<{
-          status: string;
-          message: string;
-          data: {
-            lantern_id: string;
-            owner_name: string;
-            emotion: string;
-            is_current_lantern: boolean;
-          }[];
-        }>(`/lanterns?current_lantern_id=${currentLanternId}`);
-
-        const lanternsWithRect = response.data.map((lantern) => ({
-          ...lantern,
-          rect: {
-            x: Math.random() * 600,
-            y: Math.random() * 400,
-            width: 100,
-            height: 100,
-          },
-        }));
-        setLanterns(lanternsWithRect);
-      } catch (error) {
-        console.error(error);
-        alert('다시 시도해주세요');
-      }
-    };
-
-    fetchLanterns();
-  }, [currentLanternId]);
+  const { hitLanternId } = useLanternHit(lanterns);
 
   useEffect(() => {
     if (hitLanternId) {
