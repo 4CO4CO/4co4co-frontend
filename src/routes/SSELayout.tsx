22e8aed0a558aa416b4d64cab8ba32cec97b1b95
imddoy <imddoy@duksung.ac.kr>
2025-08-09 15:33:40 +0900

    fix: entrycode 확인 로직 추가

--- src/routes/SSELayout.tsx
+++ src/routes/SSELayout.tsx
@@ -18,7 +18,7 @@ export const SSELayout = () => {
   });
 
   useEffect(() => {
-    if (isCompleted) {
+    if (isCompleted && entryCode) {
       navigate(`/lanterns?currentLanternId=${entryCode}`);
     }
   }, [isCompleted, navigate, entryCode]);
